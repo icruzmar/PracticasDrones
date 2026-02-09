dbe12b1b6099cd9b0278ae11c53e1c210c988a62
icruzmar <icruzm.indra@gmail.com>
2026-02-09 16:34:43 +0100

    Update para Front

--- drones/src/main/java/com/example/drones/controller/SimulacionController.java
+++ drones/src/main/java/com/example/drones/controller/SimulacionController.java
@@ -2,6 +2,7 @@ package com.example.drones.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -25,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SimulacionController {
     private final SimulacionService simu;
 
